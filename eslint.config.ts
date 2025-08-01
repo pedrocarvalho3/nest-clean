b465322f8e1a8a029952f5a41c7da263f6783e3c
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-08-01 11:27:41 -0300

    initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,16 @@
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  files: ["**/*.ts"],
+  plugins: {
+    prettier: eslintPluginPrettier,
+  },
+  extends: [...tseslint.configs.recommended, eslintConfigPrettier],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+});
