63f5592485b67ab17599e1bab63877896c94d98b
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-08-15 10:33:08 -0300

    finalizing prisma schema

---
+++ prisma/migrations/20250815133206_create_attachments/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "attachments" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "question_id" TEXT,
+    "answer_id" TEXT,
+
+    CONSTRAINT "attachments_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "attachments" ADD CONSTRAINT "attachments_question_id_fkey" FOREIGN KEY ("question_id") REFERENCES "questions"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "attachments" ADD CONSTRAINT "attachments_answer_id_fkey" FOREIGN KEY ("answer_id") REFERENCES "answers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
