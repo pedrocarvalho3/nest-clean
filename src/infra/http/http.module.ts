a4626770f8e56a6a85a15d555e669764e7a29a16
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-08-19 14:33:33 -0300

    controller: delete question

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -14,6 +14,8 @@ import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/g
 import { GetQuestionBySlugController } from "./controllers/get-question-by-slug.controller";
 import { EditQuestionController } from "./controllers/edit-question.controller";
 import { EditQuestionUseCase } from "@/domain/forum/application/use-cases/edit-question";
+import { DeleteQuestionController } from "./controllers/delete-question.controller";
+import { DeleteQuestionUseCase } from "@/domain/forum/application/use-cases/delete-question";
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -24,6 +26,7 @@ import { EditQuestionUseCase } from "@/domain/forum/application/use-cases/edit-q
     FetchRecentQuestionsController,
     GetQuestionBySlugController,
     EditQuestionController,
+    DeleteQuestionController,
   ],
   providers: [
     RegisterStudentUseCase,
@@ -32,6 +35,7 @@ import { EditQuestionUseCase } from "@/domain/forum/application/use-cases/edit-q
     FetchRecentQuestionsUseCase,
     GetQuestionBySlugUseCase,
     EditQuestionUseCase,
+    DeleteQuestionUseCase,
   ],
 })
 export class HttpModule {}
