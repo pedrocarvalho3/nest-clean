c7878f0758b5db291a664d45e9eef6d244ceee2e
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-08-19 15:05:17 -0300

    e2e-test: delete answer controller

--- src/infra/http/controllers/delete-answer.controller.e2e-spec.ts
+++ src/infra/http/controllers/delete-answer.controller.e2e-spec.ts
@@ -1,33 +1,67 @@
-import {
-  BadRequestException,
-  Controller,
-  Delete,
-  HttpCode,
-  Param,
-} from "@nestjs/common";
-import { CurrentUser } from "@/infra/auth/current-user.decorator";
-import { UserPayload } from "@/infra/auth/jwt.strategy";
-import { DeleteAnswerUseCase } from "@/domain/forum/application/use-cases/delete-answer";
+import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+import { AnswerFactory } from "test/factories/make-answer";
+import { QuestionFactory } from "test/factories/make-question";
+import { StudentFactory } from "test/factories/make-student";
 
-@Controller("/answers/:id")
-export class DeleteAnswerController {
-  constructor(private deleteAnswer: DeleteAnswerUseCase) {}
+describe("Delete answer (E2E)", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let studentFactory: StudentFactory;
+  let questionFactory: QuestionFactory;
+  let answerFactory: AnswerFactory;
+  let jwt: JwtService;
 
-  @Delete()
-  @HttpCode(204)
-  async handle(
-    @CurrentUser() user: UserPayload,
-    @Param("id") answerId: string,
-  ) {
-    const userId = user.sub;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile();
 
-    const result = await this.deleteAnswer.execute({
-      answerId,
-      authorId: userId,
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+    studentFactory = moduleRef.get(StudentFactory);
+    questionFactory = moduleRef.get(QuestionFactory);
+    answerFactory = moduleRef.get(AnswerFactory);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test("[DELETE] /answers/:id", async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
     });
 
-    if (result.isLeft()) {
-      throw new BadRequestException();
-    }
-  }
-}
+    const answer = await answerFactory.makePrismaAnswer({
+      authorId: user.id,
+      questionId: question.id,
+    });
+
+    const answerId = answer.id.toString();
+
+    const response = await request(app.getHttpServer())
+      .delete(`/answers/${answerId}`)
+      .set("Authorization", `Bearer ${accessToken}`);
+
+    expect(response.statusCode).toBe(204);
+
+    const answerOnDatabase = await prisma.answer.findUnique({
+      where: {
+        id: answerId,
+      },
+    });
+
+    expect(answerOnDatabase).toBeNull();
+  });
+});
